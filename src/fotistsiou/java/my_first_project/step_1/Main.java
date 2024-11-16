2a273a9b8c0a52eb54b4bdf20ec8b07deb3f0d48
fotistsiou <fotis.tsioumas@outlook.com>
2024-11-16 19:45:05 +0200

    Step 1

--- src/fotistsiou/java/my_first_project/step_1/Main.java
+++ src/fotistsiou/java/my_first_project/step_1/Main.java
@@ -1,13 +1,21 @@
 package fotistsiou.java.my_first_project.step_1;
 
 public class Main {
+
     public static void main(String[] args) {
+        int bubblegum = 2;
+        double toffee = 0.2;
+        int iceCream = 5;
+        int milkChocolate = 4;
+        double doughnut = 2.5;
+        double pancake = 3.2;
+
         System.out.println("Prices:");
-        System.out.println("Bubblegum: $2");
-        System.out.println("Toffee: $0.2");
-        System.out.println("Ice cream: $5");
-        System.out.println("Milk chocolate: $4");
-        System.out.println("Doughnut: $2.5");
-        System.out.println("Pancake: $3.2");
+        System.out.println("Bubblegum: $" + bubblegum);
+        System.out.println("Toffee: $" + toffee);
+        System.out.println("Ice cream: $" + iceCream);
+        System.out.println("Milk chocolate: $" + milkChocolate);
+        System.out.println("Doughnut: $" + doughnut);
+        System.out.println("Pancake: $" + pancake);
     }
 }
