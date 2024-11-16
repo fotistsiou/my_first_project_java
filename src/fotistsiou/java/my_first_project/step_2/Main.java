5435a697f1b267270ef7d3e9321a60f2a5d10efd
fotistsiou <fotis.tsioumas@outlook.com>
2024-11-16 19:41:10 +0200

    Step 2

---
+++ src/fotistsiou/java/my_first_project/step_2/Main.java
@@ -0,0 +1,24 @@
+package fotistsiou.java.my_first_project.step_2;
+
+public class Main {
+    public static void main(String[] args) {
+        long bubblegum = 202;
+        long toffee = 118;
+        long iceCream = 2250;
+        long milkChocolate = 1680;
+        long doughnut = 1075;
+        long pancake = 80;
+
+        System.out.println("Earned amount:");
+        System.out.println("Bubblegum: $" + bubblegum);
+        System.out.println("Toffee: $" + toffee);
+        System.out.println("Ice cream: $" + iceCream);
+        System.out.println("Milk chocolate: $" + milkChocolate);
+        System.out.println("Doughnut: $" + doughnut);
+        System.out.println("Pancake: $" + pancake);
+
+        long sum = bubblegum + toffee + iceCream + milkChocolate + doughnut + pancake;
+
+        System.out.println("Income: $" + sum);
+    }
+}
